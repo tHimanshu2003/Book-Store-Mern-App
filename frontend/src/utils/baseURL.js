5bdbc8d8070ae733936b016f1ceaabb5eebb5f3a
Himanshu Thakur <96857622+tHimanshu2003@users.noreply.github.com>
2025-07-25 23:14:34 +0530

    Update baseURL.js

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return import.meta.env.BACKEND_URI || "http://localhost:5000"
+    return import.meta.env.BACKEND_URL || "http://localhost:5000"
 }
 
 export default getBaseUrl;
