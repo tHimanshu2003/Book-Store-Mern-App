63646386c7ca94758c64c3b7720f8e06e0d1fd3c
Himanshu Thakur <96857622+tHimanshu2003@users.noreply.github.com>
2025-07-25 23:14:21 +0530

    Update getImgUrl.js

--- frontend/src/utils/getImgUrl.js
+++ frontend/src/utils/getImgUrl.js
@@ -1,5 +1,5 @@
 function getImgUrl (name) {
-    return new URL(`../assets/books/${name}`, process.env.BACKEND_URL)
+    return new URL(`../assets/books/${name}`, import.meta.env.BACKEND_URL)
 }
 
 export {getImgUrl}
